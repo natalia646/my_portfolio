0230744c4290bdefd13ff4483c3b88111314ea2a
natalia646 <75090544+natalia646@users.noreply.github.com>
2024-03-19 14:43:20 +0200

    first commit

---
+++ src/components/Header/Header.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import logo from '../../assets/logo.svg'
+
+import { Logo } from './Heder.styled'
+
+const Header = () => {
+  return (
+    <header>
+      <Logo src={logo} alt='logo'></Logo>
+      <nav>
+        
+      </nav>
+    </header>
+  )
+}
+
+export default Header
